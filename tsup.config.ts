6d0ee80dd3b1894b36e07259150e8d8dfe8a02f1
Avirup Ghosh <ag.avirup1997@gmail.com>
2024-07-21 13:39:07 +0530

    🔧 using dist

--- tsup.config.ts
+++ tsup.config.ts
@@ -4,13 +4,12 @@ import { defineConfig } from 'tsup';
 
 // start build
 export default defineConfig({
-  entry: { plugin: path.resolve(__dirname, 'src/index.ts') },
+  entry: ['src/index.ts'],
   format: ['cjs', 'esm'],
-  outDir: path.resolve(__dirname),
   onSuccess: async () => {
     // copy type definition
     let source = path.resolve(__dirname, 'src/plugin.d.ts');
-    let target = path.resolve(__dirname, 'plugin.d.ts');
+    let target = path.resolve(__dirname, 'dist/index.d.ts');
     fs.copyFileSync(source, target);
     console.log('\x1b[30mDTS plugin.d.ts');
     console.log('\x1b[30mDTS    Copied Type definitions');
